ca450bb91c49de2a4271601844764fc783ef1701
Brian Chang <kyoukamu@gmail.com>
2017-09-11 15:19:15 -0700

    finished the story

--- storyboard.py
+++ storyboard.py
@@ -1,34 +1,37 @@
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+app.secret_key = '%4#(^10304@@*80e8w7'
 
+# Load upon start
 @app.route("/")
 def initializegame():
 	return render_template("index.html")
 
+# form submit of the dragon's first choice
 @app.route("/firstchoice", methods = ["POST"])
 def firstchoice():
-	food1 = request.form["choice"]
-	if food1 == "Children":
 		return redirect("/childrenstory")
-	else:
-		return redirect("/death")
 
+#display the dragon's second choices
 @app.route("/childrenstory")
 def storytwo():
 	return	render_template("childrenstory.html")
 
+
+# form submit of the dragon's second choice
 @app.route("/secondchoice", methods =["POST"])
 def secondchoice():
-	food2 = requestion.form["choice2"]
-	if food2 == "":
-		return redirect("")
-	else:
-		return redirect("")
+		return redirect("/veganstory")
 
-@app.route("/death")
+@app.route("/veganstory")
+def win():
+	return render_template("veganstory.html")
+
+# route of the dragon when dead
+@app.route("/death", methods =["POST"])
 def dragondeath():
 	return render_template("death.html")
 
-	
+
 app.run(debug=True)
