9cf75bf1e8a9e52682ae08299bf669393852be72
Brian Chang <kyoukamu@gmail.com>
2017-09-11 14:18:09 -0700

    added more story boards

--- storyboard.py
+++ storyboard.py
@@ -7,9 +7,28 @@ def initializegame():
 	return render_template("index.html")
 
 @app.route("/firstchoice", methods = ["POST"])
+def firstchoice():
 	food1 = request.form["choice"]
 	if food1 == "Children":
 		return redirect("/childrenstory")
 	else:
 		return redirect("/death")
+
+@app.route("/childrenstory")
+def storytwo():
+	return	render_template("childrenstory.html")
+
+@app.route("/secondchoice", methods =["POST"])
+def secondchoice():
+	food2 = requestion.form["choice2"]
+	if food2 == "":
+		return redirect("")
+	else:
+		return redirect("")
+
+@app.route("/death")
+def dragondeath():
+	return render_template("death.html")
+
+	
 app.run(debug=True)
